1754d758dd2230a87bab4e991214b24c477fe0f8
BOTOOM <eddiazr@correo.udistrital.edu.co>
2019-09-19 16:03:30 -0500

    se eliminan metodos no necesarios de wrkflow, se añade modelo de alerta #2

--- controllers/workflow.go
+++ controllers/workflow.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"github.com/astaxie/beego"
+	"github.com/udistrital/nuxeo_mid/models"
 )
 
 // WorkflowController operations for Workflow
@@ -12,32 +13,27 @@ type WorkflowController struct {
 // URLMapping ...
 func (c *WorkflowController) URLMapping() {
 	c.Mapping("Post", c.Post)
-	c.Mapping("GetOne", c.GetOne)
 	c.Mapping("GetAll", c.GetAll)
 	c.Mapping("Put", c.Put)
-	c.Mapping("Delete", c.Delete)
 }
 
 // Post ...
 // @Title Create
 // @Description create Workflow
-// @Param	body		body 	models.Workflow	true		"body for Workflow content"
-// @Success 201 {object} models.Workflow
+// @Param	docID	query	string	true		"ID del documento"
+// @Success 200 {}
 // @Failure 403 body is empty
 // @router / [post]
 func (c *WorkflowController) Post() {
+	var alertErr models.Alert
+	// alertas := append([]interface{}{"Response:"})
 
-}
-
-// GetOne ...
-// @Title GetOne
-// @Description get Workflow by id
-// @Param	id		path 	string	true		"The key for staticblock"
-// @Success 200 {object} models.Workflow
-// @Failure 403 :id is empty
-// @router /:id [get]
-func (c *WorkflowController) GetOne() {
-
+	DocumentID := c.GetString("docID")
+	alertErr.Type = "OK"
+	alertErr.Code = "200"
+	alertErr.Body = DocumentID
+	c.Data["json"] = alertErr
+	c.ServeJSON()
 }
 
 // GetAll ...
@@ -65,16 +61,5 @@ func (c *WorkflowController) GetAll() {
 // @Failure 403 :id is not int
 // @router /:id [put]
 func (c *WorkflowController) Put() {
-
-}
-
-// Delete ...
-// @Title Delete
-// @Description delete the Workflow
-// @Param	id		path 	string	true		"The id you want to delete"
-// @Success 200 {string} delete success!
-// @Failure 403 id is empty
-// @router /:id [delete]
-func (c *WorkflowController) Delete() {
 
 }
--- routers/commentsRouter_controllers.go
+++ routers/commentsRouter_controllers.go
@@ -135,27 +135,9 @@ func init() {
 
     beego.GlobalControllerRouter["github.com/udistrital/nuxeo_mid/controllers:WorkflowController"] = append(beego.GlobalControllerRouter["github.com/udistrital/nuxeo_mid/controllers:WorkflowController"],
         beego.ControllerComments{
-            Method: "GetOne",
-            Router: `/:id`,
-            AllowHTTPMethods: []string{"get"},
-            MethodParams: param.Make(),
-            Filters: nil,
-            Params: nil})
-
-    beego.GlobalControllerRouter["github.com/udistrital/nuxeo_mid/controllers:WorkflowController"] = append(beego.GlobalControllerRouter["github.com/udistrital/nuxeo_mid/controllers:WorkflowController"],
-        beego.ControllerComments{
             Method: "Put",
             Router: `/:id`,
             AllowHTTPMethods: []string{"put"},
-            MethodParams: param.Make(),
-            Filters: nil,
-            Params: nil})
-
-    beego.GlobalControllerRouter["github.com/udistrital/nuxeo_mid/controllers:WorkflowController"] = append(beego.GlobalControllerRouter["github.com/udistrital/nuxeo_mid/controllers:WorkflowController"],
-        beego.ControllerComments{
-            Method: "Delete",
-            Router: `/:id`,
-            AllowHTTPMethods: []string{"delete"},
             MethodParams: param.Make(),
             Filters: nil,
             Params: nil})
