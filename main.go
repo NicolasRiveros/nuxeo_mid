897b05be306a7a219b7e762acaae9f066fc28d82
BOTOOM <eddiazr@correo.udistrital.edu.co>
2019-10-15 13:30:34 -0500

    aedicion para status

--- main.go
+++ main.go
@@ -3,10 +3,10 @@ package main
 import (
 	_ "github.com/udistrital/nuxeo_mid/routers"
 
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/plugins/cors"
 	apistatus "github.com/udistrital/utils_oas/apiStatusLib"
 	"github.com/udistrital/utils_oas/customerror"
-	"github.com/astaxie/beego/plugins/cors"
-	"github.com/astaxie/beego"
 )
 
 func main() {
@@ -14,7 +14,6 @@ func main() {
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
 	}
-	beego.Run()
 	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
 		AllowOrigins: []string{"*"},
 		AllowMethods: []string{"PUT", "PATCH", "GET", "POST", "OPTIONS", "DELETE"},
