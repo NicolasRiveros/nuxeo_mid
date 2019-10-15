5bef9fde239bfa00ce100b56416762f0d3c86c7c
BOTOOM <eddiazr@correo.udistrital.edu.co>
2019-10-15 13:17:04 -0500

    cors

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	_ "github.com/udistrital/nuxeo_mid/routers"
 
+	apistatus "github.com/udistrital/utils_oas/apiStatusLib"
+	"github.com/udistrital/utils_oas/customerror"
+	"github.com/astaxie/beego/plugins/cors"
 	"github.com/astaxie/beego"
 )
 
@@ -11,5 +14,20 @@ func main() {
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
 	}
+	beego.Run()
+	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{"PUT", "PATCH", "GET", "POST", "OPTIONS", "DELETE"},
+		AllowHeaders: []string{"Origin", "x-requested-with",
+			"content-type",
+			"accept",
+			"origin",
+			"authorization",
+			"x-csrftoken"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
+	apistatus.Init()
+	beego.ErrorController(&customerror.CustomErrorController{})
 	beego.Run()
 }
